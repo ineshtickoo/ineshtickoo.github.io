cb8241f527e2d909fdbe6edfe5a2cdf2e1c0cc92
Inesh Tickoo <inesh.tickoo@gmail.com>
2021-06-04 00:16:04 +0530

    got rid of image issues.. i guess

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ const projects = [
     {
         name: "Sahay",
         description: "bruh"
+    },
+    {
+        name: "SayLore",
+        description: "Concept fashion brand and shop UI/UX"
+    },
+    {
+        name: "Cryptocracy",
+        description: "International Cryptic Hunt - branding and platform UI"
     }
 ];
 
@@ -25,7 +33,7 @@ const projects = [
 document.querySelector('.container').innerHTML=`${
     projects.map(project => {
         return `
-            <div class="card" id="${project.name}box" style="background-image:url(./projects/${project.name}/cover.png) ">
+            <div class="card" id="${project.name}box" style="background-image:url(./projects/${project.name.toLowerCase()}/cover.png) ">
                 <div class="text" id="${project.name}text">
                     <h1> ${project.name} </h1>
                     <p>  ${project.description} </p>
