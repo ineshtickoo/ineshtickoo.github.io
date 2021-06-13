7011a1ef29cc39857895996a356bec764f2c7cc0
Inesh Tickoo <inesh.tickoo@gmail.com>
2021-06-13 12:38:56 +0530

    fixed blocks

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const projects = [
     },
     {
         name: "RediSafe",
-        description: "This is an example description",
+        description: "Modern app for monitoring your health data offline and prevent contracting COVID.",
         image: "https://i.imgur.com/RR2rEan.png"
     },
     {
         name: "Sahay",
-        description: "bruh",
+        description: "Sahay allows hospitals to work together and share resources during shortages.",
         image: "https://i.imgur.com/9x5I6Z6.png"
     },
     {
@@ -31,8 +31,8 @@ const projects = [
         image: "https://i.imgur.com/Rw19KGO.png"
     },
     {
-        name: "CORE Website",
-        description: "Computers society of Delhi Public School, Dwarka",
+        name: "CORE",
+        description: "Website for the computers society of Delhi Public School, Dwarka",
         image: "https://i.imgur.com/Ay6yWtA.png"
     }
 ];
@@ -41,12 +41,20 @@ const projects = [
 
 document.querySelector('.container').innerHTML=`${
     projects.map(project => {
-        return `
+        return (`
             <div class="card" id="${project.name}box" style="background-image:url(./projects/${project.name.toLowerCase()}/cover.png) ">
                 <div class="text" id="${project.name}text">
                     <h1> ${project.name} </h1>
                     <p>  ${project.description} </p>
                 </div>
-            </div>`
+            </div>`)
     }).join('')
 }`;
+
+
+const skills = [
+    {
+        skill: "bruh",
+        desc: "5 stars"
+    }
+]
