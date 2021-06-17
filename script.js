29cc6dbfd59648a022a6588083ba8039190482bf
Inesh Tickoo <inesh.tickoo@gmail.com>
2021-06-17 15:49:44 +0530

    added project links lmaooo

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ const projects = [
     {
         name: "Potus",
         description: "Concept Kanye West Album, Branding and UI/UX",
-        image: "https://i.imgur.com/gWxQiQB.png"
+        image: "https://i.imgur.com/gWxQiQB.png",
+        link: "https://www.behance.net/gallery/108134777/POTUS-Concept-Kanye-West-Album-and-UIUX"
     },
     {
         name: "Hlo+",
         description: "Smart hyperlocalisation and crowd control system using 5G",
-        image:"https://i.imgur.com/RR2rEan.png"
+        image:"https://i.imgur.com/RR2rEan.png",
+        link: "https://www.behance.net/gallery/119577627/Hlo-UIUX"
 
     },
     {
         name: "RediSafe",
         description: "Modern app for monitoring your health data offline and prevent contracting COVID.",
-        image: "https://i.imgur.com/RR2rEan.png"
+        image: "https://i.imgur.com/RR2rEan.png",
+        link: "https://www.behance.net/gallery/119608771/Redisafe"
     },
     {
         name: "Sahay",
         description: "Sahay allows hospitals to work together and share resources during shortages.",
-        image: "https://i.imgur.com/9x5I6Z6.png"
+        image: "https://i.imgur.com/9x5I6Z6.png",
+        link: "https://www.behance.net/gallery/118691091/Sahay-Consolidating-the-Healthcare-system?mv=email"
     },
     {
         name: "SayLore",
         description: "Concept fashion brand and shop UI/UX",
-        image: "https://i.imgur.com/9x5I6Z6.png"
+        image: "https://i.imgur.com/9x5I6Z6.png",
+        link: "https://www.behance.net/gallery/90181055/SayLor-Beauty-Products"
     },
     {
         name: "Cryptocracy",
         description: "International Cryptic Hunt - branding and platform UI", 
-        image: "https://i.imgur.com/Rw19KGO.png"
+        image: "https://i.imgur.com/Rw19KGO.png",
+        link: "https://cryptichunt.com"
     },
     {
         name: "CORE",
         description: "Website for the computers society of Delhi Public School, Dwarka",
-        image: "https://i.imgur.com/Ay6yWtA.png"
+        image: "https://i.imgur.com/Ay6yWtA.png",
+        link: "https://cryptichunt.com"
     }
 ];
 
@@ -42,12 +49,12 @@ const projects = [
 document.querySelector('.container').innerHTML=`${
     projects.map(project => {
         return (`
-            <div class="card" id="${project.name}box" style="background-image:url(./projects/${project.name.toLowerCase()}/cover.png) ">
+            <a target="__blank" href="${project.link}"><div class="card" id="${project.name}box" style="background-image:url(./projects/${project.name.toLowerCase()}/cover.png) ">
                 <div class="text" id="${project.name}text">
                     <h1> ${project.name} </h1>
                     <p>  ${project.description} </p>
                 </div>
-            </div>`)
+            </div> </a>`)
     }).join('')
 }`;
 
